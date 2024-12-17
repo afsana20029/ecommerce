687c1c92954b1ed0c1343d4e03b082f26c117dfa
USER <afsanaaktermim2792001@gmail.com>
2024-12-17 10:07:55 +0600

    Initial commit

---
+++ lib/features/auth/ui/screens/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:ecommerce/app/assets_path.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            SvgPicture.asset(
+              AssetsPath.appLogoSvg,
+              width: 80,
+            ),
+            const Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:ecommerce/app.dart';
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(const CraftyBay());
+}
